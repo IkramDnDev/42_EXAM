dde741c033af3970fb7260b6f371a7ffb475cf93
Ikram Dahhan <idahhan@e3r1p2.1337.ma>
2025-01-02 18:34:47 +0100

    hh

--- rendu/ex00/ft_strlen.c
+++ rendu/ex00/ft_strlen.c
@@ -6,14 +6,15 @@
 /*   By: idahhan <idahhan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 18:25:01 by idahhan           #+#    #+#             */
-/*   Updated: 2025/01/02 18:25:29 by idahhan          ###   ########.fr       */
+/*   Updated: 2025/01/02 18:34:28 by idahhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strlen(char *str)
+int ft_strlen(char *str)
 {
-    int i = 0;
-    while(str[i])
-        i++;
-    return (i);
+	int	i = 0;
+
+	while (str[i])
+		i++;
+	return (i);
 }
